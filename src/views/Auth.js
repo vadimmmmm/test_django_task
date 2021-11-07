31ea44e6647470f2f1add3ddb7da2e5ce9132ec1
Arthur <arthur@talent500.co>
2021-11-07 13:45:57 +0300

    Added auth

---
+++ src/views/Auth.js
@@ -0,0 +1,67 @@
+import React from "react";
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+export default function Auth(){
+    const [userName, setUserName] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    // const [redirect, setRedirect] = React.useState(false);
+
+
+    function changeUserName(e){
+        setUserName(e.target.value);
+    }
+
+    function changePassword(e){
+        setPassword(e.target.value);
+    }
+
+    function onSubmit(e){
+        e.preventDefault();
+        return axios.post(`http://0.0.0.0:8000/api/v1/token/`, {
+            'user_name': userName,
+            'password': password
+        }).then(response => {
+            if (response.status === 200){
+                const cookies = new Cookies();
+                cookies.set('access', response.data.access);
+                // setRedirect(true);
+            }
+        });
+
+    }
+
+    // if(redirect) {
+    //     return <Redirect to={{pathname: `/main`}}/>
+    // }
+
+    return(
+        <div className="container">
+            <div className="row">
+
+                <div className="col-md-offset-3 col-md-6">
+                    <form className="form-horizontal" onSubmit={onSubmit}>
+                        <span className="heading">Авторизация</span>
+                        <div className="form-group">
+                            <input type="login" className="form-control" id="inputEmail" placeholder="Login"
+                                   onChange={changeUserName} value={userName}/>
+                        </div>
+                        <div className="form-group help">
+                            <input type="password" className="form-control" id="inputPassword"
+                                   placeholder="Password"
+                                   onChange={changePassword} value={password}/>
+                        </div>
+                        <div className="form-group">
+                            <div className="main-checkbox">
+                                <input type="checkbox" value="none" id="checkbox1" name="check"/>
+
+                            </div>
+                            <button type="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    )
+}
---
+++ src/views/Nav.js
@@ -0,0 +1,44 @@
+import {Link} from 'react-router-dom';
+import React from "react";
+
+export default function Nav() {
+    return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarTogglerDemo03"
+                            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Link className="navbar-brand" to={{pathname: `/main`}}>Домой</Link>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+
+                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li className="nav-item active">
+                                <Link to={{pathname: `/my-urls`}} className="dropdown-item">My urls<span
+                                    className="sr-only"> </span></Link>
+                            </li>
+                            <li className="nav-item active">
+                                <Link to={{pathname: `/main/add`}} className="dropdown-item">Добавить запись<span
+                                    className="sr-only">(current)</span></Link>
+                            </li>
+                            <li className="nav-item active">
+                                <Link to={{pathname: `/main/update`}} className="dropdown-item">Обновить запись<span
+                                    className="sr-only">(current)</span></Link>
+                            </li>
+                            <li className="nav-item active">
+                                <Link to={{pathname: `/main/find`}} className="dropdown-item">Найти подстроку<span
+                                    className="sr-only">(current)</span></Link>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                    <form className="form-inline my-2 my-lg-0">
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Зарегистрироваться
+                            </button>
+                    </form>
+                </nav>
+            </div>
+        );
+}
