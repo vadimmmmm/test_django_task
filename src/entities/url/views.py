c2a2b3b06a3394720de15ac48c2b9486558d5ac6
Arthur <arthur@talent500.co>
2021-11-07 13:23:01 +0300

    Added permissions

--- src/entities/url/views.py
+++ src/entities/url/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import redirect
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-from entities.url.access_policy import UrlAccessPolicy
 from entities.url.models import Url
 from entities.url.serializers import UrlSerializer
 
 
-class UrlCreation(generics.ListCreateAPIView):
+class UrlCreationView(generics.ListCreateAPIView):
     serializer_class = UrlSerializer
-
-    # permission_classes = (UrlAccessPolicy,)
+    permission_classes = (IsAuthenticated,)
 
     def list(self, request, *args, **kwargs):
         return Response(status=status.HTTP_200_OK, data=self.get_queryset().values('generated_url'))
@@ -23,6 +22,7 @@ class Redirect(generics.RetrieveAPIView):
     lookup_url_kwarg = 'query'
     lookup_field = 'generated_part'
     queryset = Url.objects.all()
+    permission_classes = (IsAuthenticated,)
 
     def retrieve(self, request, *args, **kwargs):
         return redirect(self.get_object().real_url)
